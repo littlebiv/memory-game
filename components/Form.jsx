40a8fdff627f105ac7fb0df0d65ef76d4110e544
littlebiv <amirarbiv1@mail.tau.ac.il>
2026-05-25 14:56:52 +0300

    entry form layout

--- components/Form.jsx
+++ components/Form.jsx
@@ -6,6 +6,7 @@ export default function Form({ handleSubmit , handleFormChange}) {
             <form className="wrapper">
                 <div className="form__inner-container">
                     <label htmlFor="category">Choose a category:</label>
+                    <div className="form__inner-container">
                     <select name="category" id="category" onChange={handleFormChange}>
                         <option value="animals-and-nature">Animals and Nature</option>
                         <option value="food-and-drink">Food and Drink</option>
@@ -13,9 +14,11 @@ export default function Form({ handleSubmit , handleFormChange}) {
                         <option value="objects">Objects</option>
                         <option value="symbols">Symbols</option>
                     </select>
+                    </div>
                 </div>
                 <div className="form__inner-container">
                     <label htmlFor="number">Choose the number of cards:</label>
+                    <div className="form__inner-container">
                     <select type="number" name="number" id="number" onChange={handleFormChange} >
                         <option value="10">10</option>
                         <option value="20">20</option>
@@ -23,6 +26,7 @@ export default function Form({ handleSubmit , handleFormChange}) {
                         <option value="40">40</option>
                         <option value="50">50</option>
                     </select>
+                    </div>
                 </div>
                 <RegularButton handleClick={handleSubmit}>
                     Start Game
